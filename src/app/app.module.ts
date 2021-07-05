3b900f032124dc5242d8ab40d22fd878abdd3aa7
Geethanjali12 <geethanjaliganesan96@gmail.com>
2021-07-05 11:21:41 +0530

    login and register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateUserComponent } from './create-user/create-user.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateUserComponent,
     LoginComponent,
+    RegisterComponent,
     
   ],
   imports: [
