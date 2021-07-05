0e9c47339ff0e1c8d832722292be93c62ba3f7fa
Geethanjali12 <geethanjaliganesan96@gmail.com>
2021-07-05 21:35:47 +0530

    login and register in angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,22 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatDatepickerModule} from  '@angular/material/datepicker';
+import { FormsModule } from '@angular/forms';
+import {MatListModule} from '@angular/material/list';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,9 +41,26 @@ import { RegisterComponent } from './register/register.component';
     ReactiveFormsModule,
     ToastrModule.forRoot({positionClass:'toast-top-right'}),
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatSliderModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCheckboxModule, 
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatListModule,
+    FormsModule,
+    MatDatepickerModule,
+    FlexLayoutModule,
+    MatNativeDateModule,
+    MatSidenavModule
   ],
-  providers: [],
+  providers: [
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  hide = true;
 
   ngOnInit(): void {
   }
